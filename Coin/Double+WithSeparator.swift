be448e913a886989a45cc59d3f0f1685c7202d23
Jan Moravek <moravek.jan87@gmail.com>
2017-12-13 17:56:34 +0100

    functional separator

---
+++ Coin/Double+WithSeparator.swift
@@ -0,0 +1,19 @@
+//
+//  extension.swift
+//  Coin
+//
+//  Created by Jan Moravek on 12/12/2017.
+//  Copyright © 2017 Jan Moravek. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    var withSeparator:String {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = "\u{2009}"
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        return formatter.string(from: self as NSNumber)!
+    }
+}
